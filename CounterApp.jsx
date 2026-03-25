3e4c38ef9dbb8c3fc552f9ee2ada76c86449ea3a
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2026-03-25 09:43:20 +0500

    App

---
+++ CounterApp.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Simple Counter</h1>
+      <h2>{count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+export default App;
